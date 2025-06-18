3b7584be57bde3d28e61d8d0bb25d4ba6086701a
r.tatarinov <r.tatarinov@optimism.ru>
2025-06-18 18:56:00 +0300

    добавление стилей

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/main.css';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
 import AboutSection from './components/AboutSection';
