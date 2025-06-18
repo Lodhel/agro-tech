54db4d53ab1d47f9100a947602daac8231d99098
r.tatarinov <r.tatarinov@optimism.ru>
2025-06-18 18:53:45 +0300

    примеры картинок

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import team1 from '../assets/team1.jpg';
-import team2 from '../assets/team2.jpg';
-import team3 from '../assets/team3.jpg';
+import team1 from '../assets/img.png';
+import team2 from '../assets/img_1.png';
+import team3 from '../assets/img_2.png';
 
 const team = [
   { name: 'Имя Фамилия', role: 'CEO', photo: team1, email: 'name@agrotech.ru' },
