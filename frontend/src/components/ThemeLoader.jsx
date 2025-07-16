fe98fe7a5bc20b62333da64a5cafab92aea63bde
r.tatarinov <r.tatarinov@optimism.ru>
2025-07-16 19:40:29 +0300

    светлая и темная темы

--- frontend/src/components/ThemeLoader.jsx
+++ frontend/src/components/ThemeLoader.jsx
@@ -1,11 +1,12 @@
 import { useLayoutEffect } from 'react';
 
 export function useThemeLoader(theme) {
-  console.log(theme)
   useLayoutEffect(() => {
     if (theme === 'dark') {
+      console.log(theme)
       import('../styles/main.dark.css');
     } else {
+      console.log(theme)
       import('../styles/main.css');
     }
   }, [theme]);
