f8183de94e3b688ff6cb3093bf40d914ca4dfe45
r.tatarinov <r.tatarinov@optimism.ru>
2025-07-16 19:44:28 +0300

    светлая и темная темы

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './styles/main.dark.css';
+import React, { useState } from 'react';
+import { useThemeLoader } from './components/ThemeLoader';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import StatsSection from './components/StatsSection';
@@ -9,17 +9,18 @@ import JoinSection from './components/JoinSection';
 import Footer from './components/Footer';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  useThemeLoader(theme);
+
   return (
     <>
       <Header />
       <HeroSection />
       <StatsSection />
-
       <section className="gradient-wrapper">
-          <FeaturesSection />
-          <AboutSection />
+        <FeaturesSection />
+        <AboutSection />
       </section>
-
       <JoinSection />
       <Footer />
     </>
